2a669ffbb7045e51649d61326f34a24745b3f3c8
serxka <serxka@protonmail.com>
2022-03-08 21:09:19 +1100

    Updated spinlock take loop

--- src/utils/sync/spinlock.h
+++ src/utils/sync/spinlock.h
@@ -2,12 +2,12 @@
 
 #include <utils/base/defs.h>
 
-typedef volatile struct { volatile uint32_t lock[1]; } spinlock_t;
+typedef volatile uint32_t spinlock_t[1];
 
-#define spinlock_init$(l)        \
-	do {                     \
-		(l).lock[0] = 0; \
-	} while (0)
+#define spinlock_init$(l) (l)[0] = 0x0
 
-#define spinlock_take$(l) while (__sync_lock_test_and_set((l).lock, 0x01))
-#define spinlock_give$(l) __sync_lock_release((l).lock)
+#define spinlock_take$(l)                                   \
+	do {                                                \
+		__asm__ __volatile__("pause" ::: "memory"); \
+	} while (__sync_lock_test_and_set((l), 0x1))
+#define spinlock_give$(l) __sync_lock_release((l))
